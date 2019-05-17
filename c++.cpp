c77e2a697a51750989565314d90c0f2baa305a8b
HadiCyb <hadihabibi2000@gmail.com>
2019-05-17 23:04:45 +0400

    c++

--- c++.cpp
+++ c++.cpp
@@ -1,114 +1,141 @@
 #include <iostream> 
-#include <cstring>
+#include <ctime>
 using namespace std ; 
- int ctoi ( char *x , int &i )  ;
-void main () 
+ struct student 
 {
- int num1, num2, num3,k, numofplus=0; //For this example: num1 will be 46, num2 will be 23, sign will be + 
- char sign1, sign2 ; //(sign1 is +) (sign2 is <)
- char n=10 ; 
- 
-char *x = new char [n] ; 
-cout << " Enter your equation: " ; 
-cin >> x ; 
-
-  num1= ctoi(x,k) ; // Answer is num1=46, k=2 k is the number of digits in function it was i
-  x+=k ; //it will move the pointer to the sign (+)
-  numofplus+=k;//how much pluses we have till the end of main we need to collect this numbers into numofplus
-  sign1=*x ; //save plus symbol (+) into sign varriable 
-  x++ ; //will go to next number after (+)
-  numofplus++;
-  num2= ctoi(x,k) ;//Answer is num2=23, k=2
-  x+=k ;// it will move the pointer to (<)
-  numofplus+=k;
-  sign2=*x;//save plus symbol (<) into sign varriable
-  x++;//its on the answer
-  //numofplus++; after 2 row we have x-- so we dont need ++
-  num3= ctoi(x,k);//Answer is num3=195, k=3
-  x--; //go back to the sign2 (<)
- 
-  int leftpart=0;
-
-   switch ( sign1 )
-   {
-   case '+' : leftpart=num1+num2  ; 
-    break ; 
-   case '-' : leftpart=num1-num2  ; 
-    break ; 
-   case '*' : leftpart=num1*num2  ; 
-    break ; 
-   case '/' : leftpart=num1/num2  ; 
-    break ; 
-
-   default : cout<< " no operation " << endl; 
-  
-   }
-
-
-
-   switch ( sign2 )
-   {
-   case '<' : if(leftpart<num3)
-      cout<<"Congrats Brohhhh!!!!!"<<endl;
-     else if(leftpart > num3)
-     {
-       cout<<"Equation is wrong. "<<endl;
-       *x='>';//our *x pointer was on the second sign2 (<) (we want to change this sign)
-     }
-     
-     else {
-      cout<<"Equation is wrong. "<<endl;
-     *x='=';
-     }
- break;
-   case '>' :  if(leftpart>num3)
-      cout<<"Congrats Brohhhh!!!!!"<<endl;
-     else if(leftpart < num3)
-     {
-       cout<<"Equation is wrong. "<<endl;
-       *x='<';
-     }
-     
-     else
-      {
-       cout<<"Equation is wrong. "<<endl;
-     *x='=';
-     }
- break;
-   case '=' :  if(leftpart==num3)
-      cout<<"Congrats Brohhhh!!!!!"<<endl;
-     else if(leftpart < num3)
-     {
-       cout<<"Equation is wrong. "<<endl;
-       *x='<';
-     }
-     
-     else {
-      cout<<"Equation is wrong. "<<endl;
-     *x='>';
-     }
- break;
-
-   default : cout<< "Stupid operation broohhhh."<<endl; 
-   }
-   x-=numofplus;//x-= move the pointer of x into first element of array
-   cout<<"x:  "<<x<<endl;
-   
-  delete []x;
-}
- int ctoi ( char *x , int &i )//*x is char array , i will be the number of digits of current number 
-                            //i is 2 cz we have 46 (2 numbers)
+char fname[15] ;
+char lname[15] ; 
+int age ; 
+double mark ; 
+}; 
+ void input(student group[], int n);
+ void output(student group[], int n);
+ void findname(student group[], int n);
+ void findage(student group[], int n);
+ void sort(student group[], int n);
+ void main () 
  {
- int  z=0 ; //Ex: z=46 is the curent number which will be cut from array
-   i=0 ; 
 
- while ( *(x+i) >= 48 && *(x+i) <= 57 ) //while the current element in array is number 
+ const int n=3;
+ student group[n] ; 
+ 
+ char z,f;
+ bool t=true;
+
+ while (t==true)
+{
+ cout<<"press 'I'nput,'O'utput,'F'ind,'S'ort,'E'xit:  "<<endl;
+ cin>>z;
+ switch (z)
+ {
+ case 'I': 
+ case 'i':
+  input (group, n);
+  break;
+ case 'O':
+ case 'o':
+  output(group, n);
+  break;
+
+
+ case 'F':
+ case 'f':
+  cout<<"Search 'N'ame,'A'ge: ";
+  cin>>f;
+ if(f=='A'||f=='a')
+   findage(group,n);
+ else
+  if(f=='N'||f=='n') 
+   findname(group,n);
+ else
+   cout<<"Try again!!";
+  break;
+ case 'S':
+ case 's':
+ sort(group,n);
+  break;
+ case 'E':
+ case 'e':
+  t=false;
+  cout<<"GOODBYE!!!"<<endl;
+  break;
+ default:"No opertaion";
+  break;
+ }
+} 
+ }
+  void input(student group[], int n)
+{
+ srand (time(0));
+
+    for ( int i=0 ; i<n ; i++ )
+
   {
-   z=z*10 +((int) *(x+i)-48)  ; 
-   //z=converted to int number (it will b the code of this number) Miunes 48 (code of zero '0') 
-   
-   i++ ;
-   //(go to next element of array)
-  } 
-return z ;  //what ever you cut it will return FE:(46)
+ cout << " first name : "  ; 
+ cin>>group[i].fname ; 
+ cout << " last name : "  ; 
+ cin >> group[i].lname ; 
+  
+   group[i].age=rand()%60-10 ; 
+   cout << " age is : "<< group[i].age  << endl ; 
+   group[i].mark=rand()%100-10 ; 
+   cout<<" mark is : " << group[i].mark << endl ; 
+  }
+}
+   void output(student group[], int n)
+{
+ for (int i=0;i<n;i++)
+ 
+   cout << group[i].fname << '\t'<<  group[i].lname << '\t'<< group[i].age << '\t' << group[i].mark <<endl;
+  
+}
+ void findname(student group[], int n)
+ {
+  char fn[15];
+  bool f=false;
+  cout<<"Enter Name to search:";
+  cin>>fn;
+   for ( int i=0 ; i<n ; i++ )
+    if( strcmp(group[i].fname ,fn)==0)
+ {
+  cout << group[i].fname << '\t'<<  group[i].lname << '\t'<< group[i].age << '\t' << group[i].mark <<endl;
+  f=true;
+ }
+ if(f==false)
+  cout<<"No matches found!! "<<endl;
+}
+ void findage(student group[], int n)
+ {
+  int fa;
+  bool f=false;
+  cout<<"Enter Age to search:";
+  cin>>fa;
+   for ( int i=0 ; i<n ; i++ )
+    if(fa==group[i].age)
+    {
+  cout<<"Sytsem found: ";
+  cout << group[i].fname << '\t'<<  group[i].lname << '\t'<< group[i].age << '\t' << group[i].mark <<endl;
+   f=true;
+    }
+   if (f==false)
+    cout<<"No matches found!! "<<endl;
+ }
+ void sort(student group[], int n)
+ {
+  bool t=true;
+  student temp;
+  while (t==true)
+  {
+   t=false;
+   for(int i=0;i<n-1;i++)
+    if (group[i].mark<group[i+1].mark)
+    {
+     temp=group[i+1];
+     group[i+1]=group[i];
+     group[i]=temp;
+     t=true;
+    }
+  
+  }
+  cout<<"Sorted succesfully"<<endl;
  }
